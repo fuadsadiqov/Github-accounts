84aed40b22919844e21a180b7e96ac33c2fd70e3
Fuad303 <sadiqovfuad8@gmail.com>
2023-03-21 22:30:20 +0400

    some fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,25 @@ import Output from './components/Output'
 function App() {
 
   const [user, setUser] = useState('')
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(false)
   const handleChange = (e) => {
     e.preventDefault()
     fetch("https://api.github.com/users/" + user)
     .then(res => res.json())
     .then(data => setData(data))
+    console.log(data);
   }
   return (
     <div className="App">
+          <img width="50" class="logo" src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="" />
         <div className="form">
-        <form onSubmit={(e) => handleChange(e)}>
-          <input value={user} onChange={e => setUser(e.target.value)} type="text" />
-          <button type="submit">Search</button>
-        </form>
+          <form onSubmit={(e) => handleChange(e)}>
+            <input placeholder="Write github username" required value={user} onChange={e => setUser(e.target.value)} type="text" />
+            <button type="submit">Search</button>
+          </form>
         </div>
-        <Output data={data}/>
+        {!data.message && <Output data={data}/>}
+        {data.message && <p className="notFound">Not found any accounts</p>}
     </div>
  );
 }
--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -3,6 +3,7 @@ import { CiShare1 } from 'react-icons/ci'
 
 export default function Output({data}) {
     const [repos, setRepos] = useState(null)  
+    const [slicedText, setSlicedText] = useState('')
 
     useEffect(() => {
       if(data != null){
@@ -10,29 +11,32 @@ export default function Output({data}) {
         .then(res => res.json())
         .then(item => setRepos(item))
       }
-      console.log(repos);
+      if(data.created_at){
+          setSlicedText(data.created_at.slice(0, 10))
+      }
     }, [data])
 
   return (
     <>
-      {data && <div className='Output'>
+       {data && <div className='Output'>
         <div className="head">
           <div className='output-img'>{data !== null && <img width={100} src={data.avatar_url} alt="" />}</div>
             <div style={{fontSize: '18px', fontWeight: '700' }}>@{data !== null && data.login }</div>
-            <div>Creating time: {data !== null && data.created_at.slice(0, 10) }</div>
+            <div>Creating time: {data !== null && slicedText }</div>
             <div>Location: {data !== null && data.location } {data.location === null && "Not found"}</div>
             <div style={{display: 'flex', gap: '10px'}}>
               <div>Followers: {data !== null && data.followers }</div>
               <div>Following: {data !== null && data.following }</div>
             </div>
           </div>
-        {repos && <div className="repos">
+        {data.public_repos !== 0 && <div className="repos">
           <ul>
           {repos.map((repo, index) => {
               return <li key={index}>{index + 1}. {repo.name}<a href={repo.clone_url}><CiShare1 color='black' /></a></li>    
             })}
           </ul>
         </div>}
+        {data.public_repos === 0 && <p className='notFound'>Not found any repository</p>}
     </div>}
     </>
   )
